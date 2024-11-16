abefc0ea7ea1fa43cd8e9137fe30aa8cb543c62b
DylanQin4 <dylanqin4@gmail.com>
2024-11-16 16:04:24 +0300

    sprint 7

--- src/main/java/com/ETU1792/controller/FrontController.java
+++ src/main/java/com/ETU1792/controller/FrontController.java
@@ -6,17 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ETU1792.utils.Mapping;
-import com.ETU1792.utils.ModelView;
 import com.ETU1792.utils.ScannerController;
 import com.ETU1792.utils.Utils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class FrontController extends HttpServlet {
 
@@ -52,7 +48,7 @@ public class FrontController extends HttpServlet {
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter out = response.getWriter();
 
-        Mapping mapping = getMappingForUrl(request.getRequestURI());
+        Mapping mapping = Utils.getMappingForUrl(request.getRequestURI(), this.getUrlMappings());
         if (mapping == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             out.println("Error 404: URL not found.");
@@ -61,7 +57,7 @@ public class FrontController extends HttpServlet {
 
         String controllerPackage = getInitParameter("controllerPackage");
         try {
-            invokeMappedMethod(controllerPackage, mapping, request, response);
+            Utils.invokeMappedMethod(controllerPackage, mapping, request, response);
         } catch (Exception e) {
             throw new ServletException("Error while executing method : " + e.getMessage(), e);
         }
@@ -75,35 +71,6 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
-    }
-
-    // Recuperer le mapping correspondant à une URL
-    private Mapping getMappingForUrl(String url) {
-        String cleanUrl = url.split("\\?")[0];
-        return new Mapping().findMappingForUrl(this.getUrlMappings(), cleanUrl);
-    }
-
-    // Executer la methode mappee en fonction du Mapping
-    private void invokeMappedMethod(String controllerPackage, Mapping mapping, HttpServletRequest request, HttpServletResponse response)
-            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, IOException, Exception {
-        
-        Class<?> controllerClass = Class.forName(controllerPackage + "." + mapping.getClassName());
-        Method method = Mapping.findAnnotatedMethod(controllerClass, mapping.getMethodName());
-
-        if (method == null || !(method.getReturnType() == String.class || method.getReturnType() == ModelView.class)) {
-            throw new Exception("Invalid return type for method : " + mapping.getMethodName());
-        }
-
-        Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
-        List<Object> methodParameters = Utils.prepareMethodParameters(controllerInstance, method, request, response);
-
-        Object result = method.invoke(controllerInstance, methodParameters.toArray());
-
-        if (result instanceof String) {
-            response.getWriter().println("Method executed : " + result.toString());
-        } else if (result instanceof ModelView) {
-            ((ModelView) result).forwardToView(request, response);
-        }
     }
 
     // Accesseurs pour les proprietes urlMappings et controllerClasses
